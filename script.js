d54681a22e5a4facdef03bd9243cbe0606fd88cc
Timothé Tizio <t.tizio@mailbox.org>
2023-12-02 15:41:38 +0100

    Realisation de l'exercice

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const questions = [
       'En quelle année le groupe "The Beatles" a t\'il sorti le célèbre album "Sgt. Pepper\'s Lonely Hearts Club Band" ?',
     answers: [
       "1967",
-      "1974", 
-      "1962", 
+      "1974",
+      "1962",
       "1980"],
     correctAnswerIndex: 0,
   },
@@ -31,9 +31,9 @@ const questions = [
     question:
       'Dans la série de jeux video "Zelda", quel est le nom du personnage principal qu\'incarne le joueur ?',
     answers: [
-      "Zelda", 
-      "Ganon", 
-      "Tom", 
+      "Zelda",
+      "Ganon",
+      "Tom",
       "Link"],
     correctAnswerIndex: 3,
   },
@@ -41,9 +41,9 @@ const questions = [
     question:
       "Quel est le nom de la mission spatiale lunaire, menée par la NASA, dont l'équipage a du annuler son allunissage suite à une explosion pendant le voyage ?",
     answers: [
-      "Apollo 9", 
-      "Mercury 1", 
-      "Apollo 13", 
+      "Apollo 9",
+      "Mercury 1",
+      "Apollo 13",
       "Gemini 2"],
     correctAnswerIndex: 2,
   },
@@ -51,11 +51,68 @@ const questions = [
 
 /********* NE PAS MODIFIER AU DESSUS DE CETTE LIGNE *********/
 
-/*************************/
-/* Contenu du DOM chargé */
-/*************************/
-document.addEventListener("DOMContentLoaded", () => {
-  
-// démarrage du quizz
-  
-});
+
+const state = {
+    score: 0,
+    currentQuestionIndex: 0
+}
+
+displayQuestion()
+
+
+/*
+FUNCTIONS
+*/
+
+function displayNextQuestion() {
+    if(state.currentQuestionIndex == questions.length - 1) {
+        const questionPElement = document.querySelector("#question")
+        questionPElement.innerHTML = "Merci d'avoir participé"
+
+        const answersUlElement = document.querySelector("#answers")
+        answersUlElement.innerHTML = ""
+    } else {
+        state.currentQuestionIndex++
+        displayQuestion()
+    }
+}
+
+function onClickOnAnswer(answerClickedIndex) {
+    if(isCorrectAnswer(answerClickedIndex)) {
+        state.score++
+    }
+    displayScore()
+    displayNextQuestion()
+}
+
+function isCorrectAnswer(answerClickedIndex) {
+    return answerClickedIndex == questions[state.currentQuestionIndex].correctAnswerIndex
+}
+
+function displayScore() {
+    document.querySelector("#score").innerHTML = state.score
+}
+
+function displayQuestion() {
+    const questionPElement = document.querySelector("#question")
+    const answersUlElement = document.querySelector("#answers")
+
+    questionPElement.innerHTML = questions[state.currentQuestionIndex].question
+
+    const answersList = questions[state.currentQuestionIndex].answers
+
+    answersUlElement.innerHTML = ""
+    answersList.forEach((oneAnswer, index) => {
+        const newHtmlElement = document.createElement("li")
+        newHtmlElement.className = "answer"
+        newHtmlElement.innerHTML = oneAnswer
+        newHtmlElement.id = index
+
+        newHtmlElement.addEventListener("click", (event) => {
+            onClickOnAnswer(event.target.id)
+        })
+
+        answersUlElement.appendChild(newHtmlElement)
+    })
+}
+
